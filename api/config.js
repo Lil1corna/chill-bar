09221d4a46360c0d2350c721b9a323c4b146c753
Huseyin Babayev <h2oco303@gmail.com>
2026-06-11 13:26:05 +0400

    Polish admin design and upload UX

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   res.json({
     supabaseUrl: process.env.SUPABASE_URL,
-    supabaseKey: process.env.SUPABASE_ANON_KEY
+    supabaseKey: process.env.SUPABASE_ANON_KEY,
+    storageBucket: process.env.SUPABASE_STORAGE_BUCKET || 'menu-images'
   });
 }
